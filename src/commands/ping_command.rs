2914f241e6c915f7a1cf29cceff37ef325e75a42
not a cow <104355555+not-a-cowfr@users.noreply.github.com>
2026-02-25 20:34:35 -0800

    add actual ping latency to ping command

--- src/commands/ping_command.rs
+++ src/commands/ping_command.rs
@@ -2,7 +2,8 @@ use crate::types::{Context, Error};
 
 #[poise::command(slash_command, prefix_command)]
 pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
-	ctx.reply("pong!").await?;
+	ctx.reply(format!("pong! {}ms", ctx.ping().await.as_millis()))
+		.await?;
 
 	Ok(())
 }
